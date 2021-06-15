d7726481973d37fa0a5e967eaac3f026f4dc6fdc
Yejun Lee <85493705+yejun-lee@users.noreply.github.com>
2021-06-15 15:14:28 +0900

    JavaScript_class_0615
    Java basic

---
+++ 06_15_project/myScript.js
@@ -0,0 +1,13 @@
+function myFnc() {
+// 주석 1
+    document.getElementById("demo").innerHTML = "내용 변경";
+}
+
+function myFnc_2(string) {
+    document.getElementById("demo").innerHTML = string;
+}
+
+function Chg_Color() {
+// 주석 2
+    demo.style.backgroundColor = 'red';
+}
